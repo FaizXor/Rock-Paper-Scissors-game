c7ef9426f7604211cbfb424bd19e6823956e3dc5
FaizXor <faizveameer82@gmail.com>
2026-07-13 21:44:13 +0300

    Removing comment tag from playgame function

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ let computerPlayer = document.querySelector('.ComputerPlayer')
 let blackOverlay = document.getElementById('blackOverlay')
 let speechBubble = document.querySelector('.speechBubble')
 let mainContainer = document.getElementById('mainContainer')
+let startPlay = document.querySelector('.startPlay')
+let RoundText = document.querySelector('.Round')
+let roundNumChanger = document.querySelector('.Num')
 
 const bipSound = new Audio('assets/Sounds/Pickup46.wav')
 bipSound.volume = 1;
+
+RoundText.textContent = "";
+roundNumChanger.textContent = "";
 
 function callback(mutationsList, observer) {
   console.log("Mutations:", mutationsList);
@@ -76,6 +82,11 @@ function showStartButton(){
     startPlay.style.opacity = "1";
 }
 
+
+startPlay.addEventListener('onclick', () => {
+    
+})
+
 blackOverlay.addEventListener('animationend', () => {
     triggerWinnerAnimation();
 })
@@ -107,6 +118,7 @@ speechBubble.addEventListener('animationend', () => {
 })
 
 
+
 const selectingMove = ["Scissors" , "Rock" ,"Paper"];
 
 
@@ -131,46 +143,44 @@ function playGame() {
     let humanScore = 0                          //defining the human score and computer score and setting it to 0
     let computerScore = 0
 
-//function playRound(humanChoice, computerChoice){   //making comparison between the computer selection and the human input and add a point to the winner
-//    if (humanChoice === computerChoice) {
-//        alert(`it is a tie!     Computer Choice: ${computerChoice}`); 
-//    } else if ((humanChoice === "Rock") && (computerChoice === "Scissors") ) {
-//        alert(`You won Yay!     Computer Choice: ${computerChoice}`);
-//        humanScore++;
-//    } else if ((humanChoice === "Paper") && (computerChoice === "Rock")) {
-//        alert(`You won :) !     Computer Choice: ${computerChoice}`);
-//        humanScore++;
-//   } else if ((humanChoice === "Scissors") && (computerChoice === "Paper")) {
-//        alert(`You won yippee!  Computer Choice: ${computerChoice}`)
-//        humanScore++;
-//    }
+function playRound(humanChoice, computerChoice){   //making comparison between the computer selection and the human input and add a point to the winner
+    if (humanChoice === computerChoice) {
+        alert(`it is a tie!     Computer Choice: ${computerChoice}`); 
+    } else if ((humanChoice === "Rock") && (computerChoice === "Scissors") ) {
+        alert(`You won Yay!     Computer Choice: ${computerChoice}`);
+        humanScore++;
+    } else if ((humanChoice === "Paper") && (computerChoice === "Rock")) {
+        alert(`You won :) !     Computer Choice: ${computerChoice}`);
+        humanScore++;
+   } else if ((humanChoice === "Scissors") && (computerChoice === "Paper")) {
+        alert(`You won yippee!  Computer Choice: ${computerChoice}`)
+        humanScore++;
+    }
 
-//    else {
-//        alert(`Computer Won :(  Computer Choice: ${computerChoice}`)
-//        computerScore++;
-//    }
-//}
-
-
-//for (let i = 0; i < 5; i++ ){                      //adding a round system which is a 5 round
-//    const humanSelection = getHumanChoice(i , humanScore, computerScore);
-//    const computerSelection = getComputerChoice();
-//
-//    playRound(humanSelection,computerSelection) 
-//}
-
-//let winnerPlayer = ""        //defining a winner player and setting it to blank
-
-//if (humanScore > computerScore){ // Determine final match winner based on highest score
-//    winnerPlayer = "Human!"
-//} else if (computerScore > humanScore) { 
-//    winnerPlayer = "Computer!"
-//} else { // if its equal its a tie
-//    winnerPlayer = "Nobody its a tie game!"
-//}
-
-//alert(`The winner is ${winnerPlayer}`) // making a popup to tell the user who is the winner 
-
+    else {
+        alert(`Computer Won :(  Computer Choice: ${computerChoice}`)
+        computerScore++;
+    }
 }
 
-playGame();  
+
+for (let i = 0; i < 5; i++ ){                      //adding a round system which is a 5 round
+    const humanSelection = getHumanChoice(i , humanScore, computerScore);
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection,computerSelection) 
+}
+
+let winnerPlayer = ""        //defining a winner player and setting it to blank
+
+if (humanScore > computerScore){ // Determine final match winner based on highest score
+    winnerPlayer = "Human!"
+} else if (computerScore > humanScore) { 
+    winnerPlayer = "Computer!"
+} else { // if its equal its a tie
+    winnerPlayer = "Nobody its a tie game!"
+}
+
+alert(`The winner is ${winnerPlayer}`) // making a popup to tell the user who is the winner 
+
+}  
